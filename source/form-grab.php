0434542e1e8c8854481f448ad57dfc982369cf47
ghlost <john.moore1224@gmail.com>
2015-07-24 14:20:44 -0400

    More php

--- source/form-grab.php
+++ source/form-grab.php
@@ -3,9 +3,10 @@
     use Blue\Tools\Api\Client;
 
     $client = new Client('widget-generator', '16a5024e331bf5ed07606bf31dde455abb26496e', 'https://testenvironment-whichcouk.cp.bsd.net');
+    $formID = $_GET['formID'];
 
     /** @var ResponseInterface $response */
-    $response = $client->get('entities/signup_form/131');
+    $response = $client->get('entities/signup_form/'.$formID);
     $json = $response->getBody();
     echo $json;
     // $entities = json_decode($json);
